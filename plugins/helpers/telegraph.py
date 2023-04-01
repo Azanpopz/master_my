3837ff925f1a06a06eb594e5b014f34b2036d8e5
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-04-01 23:17:17 +0530

    Update telegraph.py

--- plugins/helpers/telegraph.py
+++ plugins/helpers/telegraph.py
@@ -1,46 +1,44 @@
 import os
-import shutil
-from pyrogram import Client, filters, enums
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from telegraph import upload_file
-from info import TMP_DOWNLOAD_DIRECTORY
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+#from info import TMP_DOWNLOAD_DIRECTORY
+#from plugins.helper_functions.cust_p_filters import f_onw_fliter
 from plugins.helper_functions.get_file_id import get_file_id
 
 
-@Client.on_message(
-    filters.command("telegraph") &
-    f_onw_fliter
-)
-async def telegraph(client, message):
-    replied = message.reply_to_message
+@Client.on_message(filters.command("telegraph") & filters.private)
+async def telegraph_upload(bot, update):
+    replied = update.reply_to_message
     if not replied:
-        await message.reply_text("𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙿𝙷𝙾𝚃𝙾 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝚄𝙽𝙳𝙴𝚁 𝟻𝙼𝙱.")
+        await update.reply_text("Rᴇᴩʟʏ Tᴏ A Pʜᴏᴛᴏ / Vɪᴅᴇᴏ Uɴᴅᴇʀ 5ᴍʙ")
         return
     file_info = get_file_id(replied)
     if not file_info:
-        await message.reply_text("Not supported!")
+        await update.reply_text("Nᴏᴛ Sᴜᴩᴩᴏʀᴛᴇᴅ Mᴇᴅɪᴀ Tʏᴩᴇ !")
         return
-    _t = os.path.join(
-        TMP_DOWNLOAD_DIRECTORY,
-        str(replied.id)
-    )
-    if not os.path.isdir(_t):
-        os.makedirs(_t)
-    _t += "/"
-    download_location = await replied.download(
-        _t
-    )
+    text = await update.reply_text(text="<code>Dᴏᴡɴʟᴏᴀᴅɪɴɢ Iɴ Mʏ Sᴇʀᴠᴇʀ...</code>", disable_web_page_preview=True)   
+    media = await update.reply_to_message.download()   
+    await text.edit_text(text="<code>Dᴏᴡɴʟᴏᴀᴅ Tᴏ Mʏ Sᴇʀᴠᴇʀ Is Cᴏᴍᴩʟᴇᴛᴇ. Nᴏᴡ IᴀM Uᴩʟᴏᴀᴅɪɴɢ Tᴏ Tᴇʟᴇɢʀᴀᴩʜ ...</code>", disable_web_page_preview=True)                                            
     try:
-        response = upload_file(download_location)
-    except Exception as document:
-        await message.reply_text(message, text=document)
-    else:
-        await message.reply(
-            f"Link :- <code>https://telegra.ph{response[0]}</code>",
-            disable_web_page_preview=True
-        )
-    finally:
-        shutil.rmtree(
-            _t,
-            ignore_errors=True
+        response = upload_file(media)
+    except Exception as error:
+        print(error)
+        await text.edit_text(text=f"Eʀʀᴏʀ :- {error}", disable_web_page_preview=True)       
+        return    
+    try:
+        os.remove(media)
+    except Exception as error:
+        print(error)
+        return    
+    await text.edit_text(
+        text=f"<b>Lɪɴᴋ :-</b>\n\n<code>https://graph.org{response[0]}</code>",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton(text="🔗 ᴏᴩᴇɴ ʟɪɴᴋ", url=f"https://graph.org{response[0]}"),
+            InlineKeyboardButton(text="🧚 ꜱʜᴀʀᴇ ʟɪɴᴋ", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
+            ],[
+            InlineKeyboardButton(text="✗ ᴄʟᴏsᴇ ✗", callback_data="close")
+            ]])
         )
