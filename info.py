dda774ee9ec54feeb33a3ea1e0c5d9901ec3c392
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-04-02 12:13:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,3 +119,10 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
     # Download Tutorial Button #
 HOW_TO_DOWNLOAD =  environ.get('HOW_TO_DOWNLOAD', 'https://t.me/linkdownlos/2')
+
+    # Auto Delete For Group Message (Self Delete) #
+
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 50))
+SELF_DELETE = environ.get('SELF_DELETE', True)
+if SELF_DELETE == "True":
+    SELF_DELETE = True
